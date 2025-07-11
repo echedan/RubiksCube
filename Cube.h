a1a1768b390ca065ccd01d9b751da22fa388235c
saad01syed <saad01syed@gmail.com>
2025-07-11 16:39:13 -0500

    Update Cube.h

--- Cube.h
+++ Cube.h
@@ -1,1 +1,46 @@
+#ifndef CUBE_H
+#define CUBE_H
 
+class Cube{
+  public:
+    Cube(vector<vector<vector<char>>> newFaces); // read the input from utilities class then use it to init the cube
+    void applyMove(string move); // selects one of the moves below
+    bool isSolved(); // checks if every side is one color
+    string getStateHash(); // not really sure what this one is for
+
+    // -------------------------------------------------------
+    // 18 possible moves. I tried to make the names make sense
+    // -------------------------------------------------------
+
+    // The layer that you are looking at and the 2 layers behind it
+    void layer_facing_you_R();
+    void layer_facing_you_L();
+    void middle_wide_layer_R();
+    void middle_wide_layer_L();
+    void back_layer_R();
+    void back_layer_L();
+
+    // Horizontal layers
+    void top_horizontal_R();
+    void top_horizontal_L();
+    void middle_horizontal_R();
+    void middle_horizontal_L();
+    void bottom_horizontal_R();
+    void bottom_horizontal_L();
+
+    // Vertical layers (they go up and down, not left and right)
+    void left_vertical_U();
+    void left_vertical_D();
+    void middle_vertical_U();
+    void middle_vertical_D();
+    void right_vertical_U();
+    void right_vertical_D();
+    
+  private:
+    static constexpr int NUM_FACES = 6;
+    static constexpr int ROWS = 3;
+    static constexpr int COLS = 3;
+    vector<vector<vector<char>>> cubeFaces = vector<vector<vector<char>>>(NUM_FACES, vector<vector<char>>(ROWS, vector<char>(COLS, '.')));
+};
+
+#endif
